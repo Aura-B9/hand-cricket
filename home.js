d8342b844bbadba4e19e87baff72b9ced26eea4e
Aura-β9 <forfoxy027@gmail.com>
2025-05-11 02:48:56 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,232 +1,292 @@
-let overs = 0.5;
+"use strict";
+let overs = 1;
 let chance = overs * 6 ;
 let Swick = 2; //selected wick 
 let Dwick = 0; // done wick
 let Druns = 0; // done runs
 let Sruns = 10;//selected runs
 let click = 0;
+let user;
 
 let num1 , num2;
 
 function gamePlay(){
-    num1 = Math.floor(Math.random()*7) ;
-    num2 = Math.floor(Math.random()*7) ;
+    num1 = Math.floor((Math.random()*7));
+    num2 = Math.floor((Math.random()*7));
 }
 
 document.getElementById("btn0").onclick = function(){
-    user = 0;
-    click++;
-    gamePlay();
+   
+    if (click < chance){
 
-    if(user == num1 || user == num2){
+        user = 0;
+        click++;
+        gamePlay();
+
+        if(user == num1 || user == num2){
         Dwick++;
-    }
-    else{
+        }
+        else{
         Druns = user + Druns;
-    }
+        }
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
 
-    if(user == num1){
+        if(user == num1){
         document.getElementById("compR").textContent = num1;
-    }
-    else if(user == num2){
+        }
+        else if(user == num2){
         document.getElementById("compR").textContent = num2;        
-    }
-    else{
+        }
+        else{
         document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
+    }
+    
+    else {
+        console.log("game over!!");
     }
 }
 document.getElementById("btn1").onclick = function(){
-    user = 1;
-    click++;
-    gamePlay();
+        
+    if (click < chance){
+    
+        user = 1;
+        click++;
+        gamePlay();
 
-    if(user == num1 || user == num2){
-        Dwick++;
-    }
-    else{
-        Druns = user + Druns;
-    }
+        if(user == num1 || user == num2){
+            Dwick++;
+        }
+        else{
+            Druns = user + Druns;
+        }
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
 
-    if(user == num1){
-        document.getElementById("compR").textContent = num1;
+        if(user == num1){
+            document.getElementById("compR").textContent = num1;
+        }
+        else if(user == num2){
+            document.getElementById("compR").textContent = num2;        
+        }
+        else{
+            document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
     }
-    else if(user == num2){
-        document.getElementById("compR").textContent = num2;        
-    }
-    else{
-        document.getElementById("compR").textContent = num1;
+    else {
+        console.log("game over!!");
     }
 }
 document.getElementById("btn2").onclick = function(){
-    user = 2;
-    click++;
-    gamePlay();
 
-    if(user == num1 || user == num2){
-        Dwick++;
-    }
-    else{
-        Druns = user + Druns;
-    }
+    if (click < chance){
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        user = 2;
+        click++;
+        gamePlay();
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        if(user == num1 || user == num2){
+            Dwick++;
+        }
+        else{
+            Druns = user + Druns;
+        }
 
-    if(user == num1){
-        document.getElementById("compR").textContent = num1;
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
+
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
+
+        if(user == num1){
+            document.getElementById("compR").textContent = num1;
+        }
+        else if(user == num2){
+            document.getElementById("compR").textContent = num2;        
+        }
+        else{
+            document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
     }
-    else if(user == num2){
-        document.getElementById("compR").textContent = num2;        
-    }
-    else{
-        document.getElementById("compR").textContent = num1;
+    else {
+        console.log("game over!!");
     }
 }
 document.getElementById("btn3").onclick = function(){
-    user = 3;
-    click++;
-    gamePlay();
 
-    if(user == num1 || user == num2){
-        Dwick++;
-    }
-    else{
-        Druns = user + Druns;
-    }
+    if (click < chance){
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        user = 3;
+        click++;
+        gamePlay();
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        if(user == num1 || user == num2){
+            Dwick++;
+        }
+        else{
+            Druns = user + Druns;
+        }
 
-    if(user == num1){
-        document.getElementById("compR").textContent = num1;
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
+
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
+
+        if(user == num1){
+            document.getElementById("compR").textContent = num1;
+        }
+        else if(user == num2){
+            document.getElementById("compR").textContent = num2;        
+        }
+        else{
+            document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
     }
-    else if(user == num2){
-        document.getElementById("compR").textContent = num2;        
-    }
-    else{
-        document.getElementById("compR").textContent = num1;
+    else {
+        console.log("game over!!");
     }
 }
 document.getElementById("btn4").onclick = function(){
-    user = 4;
-    click++;
-    gamePlay();
 
-    if(user == num1 || user == num2){
-        Dwick++;
-    }
-    else{
-        Druns = user + Druns;
-    }
+    if (click < chance){
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        user = 4;
+        click++;
+        gamePlay();
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        if(user == num1 || user == num2){
+            Dwick++;
+        }
+        else{
+            Druns = user + Druns;
+        }
 
-    if(user == num1){
-        document.getElementById("compR").textContent = num1;
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
+
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
+
+        if(user == num1){
+            document.getElementById("compR").textContent = num1;
+        }
+        else if(user == num2){
+            document.getElementById("compR").textContent = num2;        
+        }
+        else{
+            document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
     }
-    else if(user == num2){
-        document.getElementById("compR").textContent = num2;        
-    }
-    else{
-        document.getElementById("compR").textContent = num1;
+    else {
+        console.log("game over!!");
     }
 }
 document.getElementById("btn5").onclick = function(){
-    user = 5;
-    click++;
-    gamePlay();
 
-    if(user == num1 || user == num2){
-        Dwick++;
-    }
-    else{
-        Druns = user + Druns;
-    }
+    if (click < chance){
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        user = 5;
+        click++;
+        gamePlay();
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        if(user == num1 || user == num2){
+            Dwick++;
+        }
+        else{
+            Druns = user + Druns;
+        }
 
-    if(user == num1){
-        document.getElementById("compR").textContent = num1;
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
+
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
+
+        if(user == num1){
+            document.getElementById("compR").textContent = num1;
+        }
+        else if(user == num2){
+            document.getElementById("compR").textContent = num2;        
+        }
+        else{
+            document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
     }
-    else if(user == num2){
-        document.getElementById("compR").textContent = num2;        
-    }
-    else{
-        document.getElementById("compR").textContent = num1;
+    else {
+        console.log("game over!!");
     }
 }
 document.getElementById("btn6").onclick = function(){
-    user = 6;
-    click++;
-    gamePlay();
 
-    if(user == num1 || user == num2){
-        Dwick++;
-    }
-    else{
-        Druns = user + Druns;
-    }
+    if (click < chance){
 
-    console.log(`runs: ${Druns}`);
-    console.log(`wicket: ${Dwick}`);
-    console.log(`user: ${user}`);
-    console.log(`Comp: ${num1 },${num2}`);
+        user = 6;
+        click++;
+        gamePlay();
 
-    document.getElementById("userR").textContent = user;
-    document.getElementById("totalR").textContent = Druns;
-    document.getElementById("totalW").textContent = Dwick;
+        if(user == num1 || user == num2){
+            Dwick++;
+        }
+        else{
+            Druns = user + Druns;
+        }
 
-    if(user == num1){
-        document.getElementById("compR").textContent = num1;
+        console.log(`runs: ${Druns}`);
+        console.log(`wicket: ${Dwick}`);
+        console.log(`user: ${user}`);
+        console.log(`Comp: ${num1 },${num2}`);
+
+        document.getElementById("userR").textContent = user;
+        document.getElementById("totalR").textContent = Druns;
+        document.getElementById("totalW").textContent = Dwick;
+
+        if(user == num1){
+            document.getElementById("compR").textContent = num1;
+        }
+        else if(user == num2){
+            document.getElementById("compR").textContent = num2;        
+        }
+        else{
+            document.getElementById("compR").textContent = num1;
+        }
+        console.log(`click:${click}`);
     }
-    else if(user == num2){
-        document.getElementById("compR").textContent = num2;        
-    }
-    else{
-        document.getElementById("compR").textContent = num1;
+    else {
+        console.log("game over!!");
     }
 }
+
